a94f93a5b972b4eadadf05063337b75f4259ce77
root <root@localhost.localdomain>
2019-10-31 07:43:55 -0600

    fix 1

--- src/index.js
+++ src/index.js
@@ -5,7 +5,11 @@ const app = express();
 
 app.set('port', process.env.PORT || 4000);
 
-app.use('/', express.static(__dirname + '/public'));
+app.use('/',(req,res) => {
+    res.send('HW');
+});
+
+app.use('/game', express.static(__dirname + '/public'));
 console.log(__dirname)
 app.listen(app.get('port'),()=>{
     console.log('Servidor en el puerto: ',app.get('port'));
