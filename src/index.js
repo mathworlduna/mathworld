a4735c9b3254c47c75613a87a6eb98d93f1a6d30
root <root@localhost.localdomain>
2019-10-31 07:53:54 -0600

    morgan

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 // modulos.
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
-app.set('port', process.env.PORT || 4000);
+app.set('port', process.env.PORT || 4001);
 
-app.use('/',(req,res) => {
-    res.send('HW');
-});
+app.use(morgan('dev'));
 
-app.use('/game', express.static(__dirname + '/public'));
+app.use('/', express.static(__dirname + '/public'));
+
 console.log(__dirname)
+
 app.listen(app.get('port'),()=>{
     console.log('Servidor en el puerto: ',app.get('port'));
 })
